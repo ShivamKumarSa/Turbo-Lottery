1fc9516778053c59c065c94ee304643b94103ba5
ShivamKumarSa <shivam.sachdeva@block8.com>
2022-10-17 20:13:57 +0530

    front

--- apps/lottery/src/Authentication/AdminAuth.tsx
+++ apps/lottery/src/Authentication/AdminAuth.tsx
@@ -4,13 +4,13 @@ import { Navigate } from 'react-router-dom';
 const AdminAuth = ({ children }: { children: JSX.Element }) => {
   const userType = localStorage.getItem('userType');
   if (userType === 'Admin') {
-    console.log('Trying to render Admin auth..');
+    // console.log('Trying to render Admin auth..');
     return children;
   }
   if (userType === 'User') {
     return <Navigate to="/dashboard" />;
   }
-  return <Navigate to="/" />;
+  return <Navigate to="/login" />;
 };
 
 export default AdminAuth;
